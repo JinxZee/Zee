cd82cd68f1766925d1dbbf145dd7d8a7a652c1b5
JinxZee <132984321+JinxZee@users.noreply.github.com>
2023-05-15 11:00:32 +0200

    Add files via upload

---
+++ PHP OPGAVE 2.php
@@ -0,0 +1,52 @@
+<?php
+
+
+////////////////////////////////////////////////////////////////////////////////////////////
+
+//Maak de onderstaande code af;
+//$variabele1 = 10;
+//$variabele2 = 10;
+
+
+
+//if($antwoord = $variabele1 == $variabele2) {
+  //  echo "De twee waarden zijn gelijk";
+//}
+
+/////////////////////////////////////////////////////////////////////////////////
+
+
+//Comment de bovenstaande code en ga verder;
+
+//$variabele1 = 10;
+//$variabele2 = 10;
+
+//if($antwoord = $variabele1 != $variabele2) {
+   // echo "De twee waarden zijn ongelijk";
+//}
+
+
+////////////////////////////////////////////////////////
+
+//$variabele1 = 10;
+//$variabele2 = 10;
+
+
+ // if($antwoord = $variabele1 == $variabele2) {
+  //echo "De twee waarden zijn gelijk";
+//} 
+
+//else{
+
+  // echo "De twee waarden zijn ongelijk";
+//}
+
+
+
+//Maak een formulier met 2 input fields (username en password) en een knopje aan.
+///Zorg voor dat de ingevoerde data wordt geprint als er op het knopje wordt gedrukt.  
+
+
+
+
+?>
