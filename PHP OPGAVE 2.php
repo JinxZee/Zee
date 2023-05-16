18642cbac5d9350af276d2784298ef92b8e8983b
JinxZee <132984321+JinxZee@users.noreply.github.com>
2023-05-16 09:07:20 +0200

    Update PHP OPGAVE 2.php

--- PHP OPGAVE 2.php
+++ PHP OPGAVE 2.php
@@ -43,10 +43,47 @@
 
 
 
+//////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
 //Maak een formulier met 2 input fields (username en password) en een knopje aan.
 ///Zorg voor dat de ingevoerde data wordt geprint als er op het knopje wordt gedrukt.  
 
 
 
 
-?>
+
+//if(isset($_POST['knopje'])){
+//   $username = $_POST["username"];
+// $password = $_POST["password"];
+
+///echo  $_POST["username"] . " " . $_POST["password"];
+//}
+
+
+//?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>POST</title>
+</head>
+<body>
+    
+
+
+<form method="POST">
+
+<label for="username">Username</label>
+<input type="text" name="username" >
+
+<label for="password">Password</label>
+<input type="password" name="password">
+
+<input type="submit" name="knopje" value="Submit">
+
+</form>
+
+
+</body>
+</html>
