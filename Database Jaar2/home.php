03fa1e26bfd9c6ae80eae540541eb3d59b09743d
Zee Babar <132984321+JinxZee@users.noreply.github.com>
2023-11-24 15:08:06 +0100

    Add files via upload

--- Database Jaar2/home.php
+++ Database Jaar2/home.php
@@ -1,8 +1,49 @@
 <?php 
 
-include 'db.php';
+include 'connectie.php';
+
 $db = new Database();
 
-    $som = $db->optellen(5,10);
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    try {
+        $db->data($_POST["email"], $_POST["password"]);
+        echo "Successfull";
+    } catch (Exception $e) {
+        echo $e->getMessage();
+    }
+}
+
+
+
 
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+<form method="POST">
+<label for="email">Email</label>
+<input type="email" name="email" >
+<label for="password">Password</label>
+<input type="password" name="password">
+<input type="submit" value="submit">
+
+
+
+
+
+</form>
+
+
+
+
+</body>
+</html>
+
+
